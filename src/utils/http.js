8feb34ef6d412740d88dfb7e81795d5885746aca
gty <1262583075@qq.com>
2025-04-09 15:38:43 +0800

    完成了请求携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 // 引入 element-plus 的消息提示组件
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import { useUserStore } from '@/store/user'
+
 
 // 创建axios实例
 const http = axios.create({
@@ -11,6 +13,13 @@ const http = axios.create({
 
 // axios请求拦截器
 http.interceptors.request.use(config => {
+  // 从 pinia中获取 token
+  const userStore = useUserStore()
+  const token = userStore.userInfo.token
+  // 如果有token，就添加到请求头中
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 }, e => Promise.reject(e))
 
