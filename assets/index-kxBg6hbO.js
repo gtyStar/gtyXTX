679635c089893030d61170d1edbd462becff2a6f
gty <1262583075@qq.com>
2025-04-17 20:27:46 +0800

    Updates

--- assets/index-kxBg6hbO.js
+++
@@ -1,1 +0,0 @@
-import{L as o,v as a}from"./loading-jg5AAb9y.js";const n={install(i){i.directive("loading",a),i.config.globalProperties.$loading=o},directive:a,service:o};export{n as E};
--- assets/pay-CevftxjM.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-CasjIeWE.js";const o=r=>e({url:`/member/order/${r}`});export{o as g};
